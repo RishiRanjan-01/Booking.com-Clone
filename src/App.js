4a977373ebddebb94af3106cd4ca0a83c9aed097
Rishi Ranjan <rishibhumi000@gmail.com>
2022-07-20 21:33:52 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <>
-    <Navbar/>
+    <HomePage/>
     </>
   );
 }
