3b2e1ae11073844d9e20ff9dde9f322832f8eb03
Rishi Ranjan <rishibhumi000@gmail.com>
2022-07-20 11:13:26 +0530

    Initial App Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
+}
+
+export default App;
