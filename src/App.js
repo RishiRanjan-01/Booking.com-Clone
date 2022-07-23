14e4a7012a1fdc67356ddb594547d6a10ea1ce36
RaviRanjan02 <raviranjan1140@gmail.com>
2022-07-23 07:23:46 +0530

    fw15_450_day_4

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import './App.css';
-import FligthOption from './Pages/FlightOption/FligthOption';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Hotel from './Pages/All_Hotel/Hotel';
-import FlightHome from "./Pages/FlightHome/FlightHome"
-
-import ContactFlight from './Pages/FlightOption/ContactFlight';
-
-import HomePage from './Pages/Home/HomePage';
-import HotelPrice from './Pages/All_Hotel/HotelPrice';
-import HotelSeeAvialability from './Pages/All_Hotel/HotelSeeAvialability';
-
-
-
+import "./App.css";
+import AllHotelsRoutes from "./Pages/All_Hotel/AllHotelsRoutes";
 
 function App() {
   return (
     <>
-      {/* <FligthOption /> */}
-      <ContactFlight />
-      {/* <HomePage/> */}
-      {/* <Hotel/> */}
-      {/* <FlightHome/> */}
-     {/* <FligthOption /> */}
-    {/* <HomePage/> */}
-    {/* <Hotel/> */}
-    {/* <HotelPrice/> */}
-    <HotelSeeAvialability/>
-    {/* <FlightHome/> */}
-
-
+      <AllHotelsRoutes />
     </>
   );
 }
